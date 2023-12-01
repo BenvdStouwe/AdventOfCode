b6ef9eb25e2e18a1e511ee9d8344290c9a3d8afc
Ben van der Stouwe <b.vanderstouwe@care-expert.nl>
2023-12-01 09:27:37 +0100

    Move to directories, add some 2020 tests and start with 2023

---
+++ 2020/AoC2023Tests/StringExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace AoC2023Tests;
+
+public static class StringExtensions
+{
+    public static string[] Parse(this string val) =>
+        val.Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+}
---
+++ 2023/AoC2023.CSharp/Extensions/StringExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace AoC2023.CSharp.Extensions;
+
+public static class StringExtensions
+{
+    public static string[] Parse(this string input) => input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+}
